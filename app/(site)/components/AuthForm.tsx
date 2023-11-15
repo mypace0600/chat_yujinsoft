a40a9b2e333f59a1094f71ed4a7e04a90563da02
mypace0600 <mypace0600@gmail.com>
2023-11-15 23:20:57 +0900

    30.42 까지 봄

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,81 @@
+'use client';
+
+import Input from "@/app/components/Input/input";
+import { useCallback, useState } from "react";
+import { FieldValue, FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+type Variant = 'LOGIN' | 'REGISTER';
 const AuthForm = () =>{
+    const [variant, setVariant] = useState<Variant>('LOGIN');
+    const [loading, setIsLoading] = useState(false);
+
+    const toggleVariant = useCallback(()=>{
+        if(variant === 'LOGIN'){
+            setVariant("REGISTER");
+        } else {
+            setVariant("LOGIN");
+        }
+    },[variant]);
+
+    const {
+        register,
+        handleSubmit,
+        formState:{
+            errors
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit : SubmitHandler<FieldValues>  = (data) => {
+        setIsLoading(true);
+        if(variant === 'REGISTER'){
+            //axios Register
+        }
+
+        if(variant === 'LOGIN'){
+            // nextauth sign in
+        }
+    }
+
+    const socialAction = (action: string) =>{
+        setIsLoading(true);
+        // nextauth social sign in
+
+    }
+
     return (
-        <div>Auth Form!</div>
+        <div
+            className="
+                mt-8
+                sm:mx-auto
+                sm:w-full
+                sm:max-w-md
+            "
+        >
+            <div
+                className="
+                    bg-white
+                    px-4
+                    py-8
+                    shadow
+                    sm:round-lg
+                    sm:px-10
+                "
+            >
+                <form
+                    className="space-y-6"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <Input />
+
+                </form>
+            </div>
+        </div>
     );
 }
 
---
+++ app/components/Input/input.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import clsx from 'clsx';
+import {
+    FieldErrors,
+    FieldValues,
+    UseFormRegister
+} from 'react-hook-form';
+
+interface InputProps {
+    label:string,
+    id:string,
+    type?:string,
+    required?:boolean,
+    register: UseFormRegister<FieldValues>,
+    errors:FieldErrors,
+    disabled?:boolean
+}
+
+const Input:React.FC<InputProps> = ({
+    label,
+    id,
+    type,
+    required,
+    register,
+    errors,
+    disabled
+}) =>{
+    return (
+        <div>
+            <label htmlFor={id}></label>
+        </div>
+    );
+}
+
+export default Input;
