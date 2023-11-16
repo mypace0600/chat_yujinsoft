223348cb4279e369d557c73b3c2c77dcdb08ffa2
mypace0600 <mypace0600@gmail.com>
2023-11-16 22:56:25 +0900

    50.57 로그인 화면 구현 중

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import Input from "@/app/components/Input/input";
+import Button from "@/app/components/Button";
+import Input from "@/app/components/Input/Input";
 import { useCallback, useState } from "react";
 import { FieldValue, FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 type Variant = 'LOGIN' | 'REGISTER';
 const AuthForm = () =>{
     const [variant, setVariant] = useState<Variant>('LOGIN');
-    const [loading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const toggleVariant = useCallback(()=>{
         if(variant === 'LOGIN'){
@@ -71,9 +72,75 @@ const AuthForm = () =>{
                     className="space-y-6"
                     onSubmit={handleSubmit(onSubmit)}
                 >
-                    <Input />
-
+                    {variant === 'REGISTER' && (
+                        <Input 
+                        id="name" 
+                        label="name" 
+                        register={register}
+                        errors={errors}
+                        />
+                    )}
+                    <Input 
+                        id="email" 
+                        label="Email address" 
+                        type="email"
+                        register={register}
+                        errors={errors}
+                    />
+                    <Input 
+                        id="password" 
+                        label="Password"
+                        type="password" 
+                        register={register}
+                        errors={errors}
+                    />
+                    <div>
+                        <Button
+                            disabled={isLoading}
+                            fullWidth
+                            type="submit"
+                        >
+                            {variant === 'LOGIN' ? 'Sign in' : 'Sign up'}
+                        </Button>
+                    </div>
                 </form>
+                <div className="mt-6">
+                    <div className="relative">
+                        <div className="
+                            absolute
+                            inset-0
+                            flex
+                            items-center
+                        ">
+                            <div className="
+                                w-full 
+                                border-t 
+                                border-gray-300
+                            "/>
+                        </div>
+                        <div className="
+                            relative
+                            flex
+                            justify-center
+                            text-sm
+                        ">
+                            <span className="
+                                bg-white
+                                px-2
+                                text-gray-500
+                            ">
+                                Or continue with
+                            </span>
+                        </div>
+                    </div>
+                    <div className="
+                        mt-6
+                        flex
+                        gap-2
+                    ">
+                        <AuthSocialButton />
+                    </div>
+                </div>
             </div>
         </div>
     );
